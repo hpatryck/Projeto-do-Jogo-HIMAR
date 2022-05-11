9662c9160ff3897f5f106297f1e9331e1614bc0e
Robson Luan <64618248+RobT0g@users.noreply.github.com>
2022-05-11 17:42:25 -0300

    Adicionei os players

--- script/Control.gd
+++ script/Control.gd
@@ -2,8 +2,32 @@ extends Control
 
 
 func _ready():
-	pass
+	var txt = get_not_text()
+	var text = txt.split(".", false)
+	var players = []
+	var sortplayers = []
+	for i in text:
+		players = (i.split(",", false))
+		if(len(sortplayers) > 0):
+			var ver = false
+			for j in range(len(sortplayers)):
+				if int(sortplayers[j][1]) < int(players[1]):
+					sortplayers.insert(j, players)
+					ver = true
+					break
+			if(!ver):
+				sortplayers.insert(len(sortplayers), players)
+		else:
+			sortplayers.append(players)
+	print(sortplayers)
 
+
+func get_not_text():
+	var file = File.new()
+	file.open("res://SaveGame.txt", File.READ)
+	var content = file.get_as_text()
+	file.close()
+	return content
 
 func _on_button_voltar_pressed():
 	get_tree().change_scene("res://scenes/menu_principal.tscn")
--- script/novojogo.gd
+++ script/novojogo.gd
@@ -1,14 +1,46 @@
 extends Control
 
+const player = [-1]
+
 func _ready():
-	pass 
+	pass
 
 	
 func _on_TextureButton_pressed():
+	var txt = $Sprite/TextEdit1.get_text()
+	var text = get_not_text()
+	var note = text.split(".", false)
+	var players = []
+	for i in range(len(note)):
+		players.append(note[i].split(",", false))
+		if players[i][0] == txt:
+			player[0] = i
+			print(player)
+	if player[0] == -1:
+		players.append([txt, 0])
+		player[0] = len(players)
+		text += ("%s," % players[-1][0]) + ("%s." % players[-1][1])
+		save(text)
 	get_tree().change_scene("res://scenes/tela_grajau.tscn")
-	pass
+	
+	#save(txt)
+	#print(txt.split("."))
 
+func save(content):
+	var file = File.new()
+	file.open("res://SaveGame.txt", File.WRITE)
+	file.store_string(content)
+	file.close()
+
+func get_not_text():
+	var file = File.new()
+	file.open("res://SaveGame.txt", File.READ)
+	var content = file.get_as_text()
+	file.close()
+	return content
 
 func _on_button_voltar_pressed():
 	get_tree().change_scene("res://scenes/comecar.tscn")
 	pass # Replace with function body.
+
+export var jogador = player
